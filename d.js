35b681d9be19beba97b2b22d7efe4b1fb7cf1b8c
GhostMobin <s1810677110@ru.ac.bd>
2023-09-02 14:12:15 +0600

    sorting 2

--- d.js
+++ d.js
@@ -6,3 +6,60 @@
 // if (!a.length) {
 //     console.log("hola");
 // }
+
+// <!--
+//             <p class="text-xl mb-4">
+//                 REST (Representational State Transfer) API is an architectural style for designing networked
+//                 applications.
+//                 It's based on a set of principles and constraints that emphasize a stateless client-server interaction,
+//                 scalability, and simplicity. Some key characteristics and concepts of REST APIs include:
+
+//             </p>
+//             <ul class="text-xl list-disc ml-5">
+
+//                 <li class="mb-4">
+//                     <bold class="font-semibold mt-6">Statelessness:</bold> Each request from a client to a server must
+//                     contain all the information needed to
+//                     understand
+//                     and fulfill the request. The server should not rely on any previous requests or stored client state.
+
+//                 </li>
+
+//                 <li class="mb-4">
+
+//                     <bold class="font-semibold mt-6">Resources:</bold> Resources are the key abstractions in REST. Each
+//                     resource is identified by a unique URI
+//                     (Uniform
+//                     Resource Identifier) and can be manipulated using standard HTTP methods like GET, POST, PUT, DELETE.
+//                 </li>
+
+//                 <li class="mb-4">
+
+//                     <bold class="font-semibold mt-6">HTTP Methods:</bold> REST APIs use standard HTTP methods for CRUD
+//                     (Create, Read, Update, Delete) operations
+//                     on
+//                     resources. For example, GET for retrieving data, POST for creating new data, PUT for updating data,
+//                     and
+//                     DELETE for removing data.
+//                 </li>
+
+//                 <li class="mb-4">
+
+//                     <bold class="font-semibold mt-6">Stateless Communication:</bold> REST APIs use stateless
+//                     communication, meaning that each request from a
+//                     client to a
+//                     server is independent, and the server doesn't maintain any client state between requests. This makes
+//                     the API
+//                     scalable and easy to cache.
+//                 </li>
+
+//                 <li class="mb-4">
+
+//                     <bold class="font-semibold mt-6">JSON or XML:</bold> Data is typically exchanged between the client
+//                     and server in a format like JSON or XML,
+//                     making
+//                     it easy for different systems to understand and work with the data.
+//                 </li>
+
+//             </ul>
+//         -->
